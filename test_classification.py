6c8b936971b1c85981355226ebd09ddf6b5be87e
Mathieu Nguyen <92989783+MathieuNg@users.noreply.github.com>
2024-05-08 10:10:31 +0200

    Update test_classification.py

--- test_classification.py
+++ test_classification.py
@@ -20,7 +20,7 @@ sys.path.append(os.path.join(ROOT_DIR, 'models'))
 def parse_args():
     '''PARAMETERS'''
     parser = argparse.ArgumentParser('Testing')
-    parser.add_argument('--use_cpu', action='store_true', default=True, help='use cpu mode')
+    parser.add_argument('--use_cpu', action='store_true', default=False, help='use cpu mode')
     parser.add_argument('--gpu', type=str, default='0', help='specify gpu device')
     parser.add_argument('--batch_size', type=int, default=24, help='batch size in training')
     parser.add_argument('--num_category', default=40, type=int, choices=[10, 40],  help='training on ModelNet10/40')
